351357fe3814775eda4fdc34c1d4ba54ed3e9fe5
wdominguez <wyattd2689@gmail.com>
2019-08-19 11:16:23 -0400

    debug off threaded on

--- textbookWebsite.py
+++ textbookWebsite.py
@@ -219,4 +219,4 @@ if __name__ == "__main__":
     # app.logger.addHandler(streamhandler)
     # app.logger.setLevel(logging.DEBUG)
     # app.run(host='0.0.0.0', port=8080, threaded=True)
-    app.run(debug=True)
+    app.run(debug=False, threaded=True)
